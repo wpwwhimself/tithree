1409b721340b77a62a96afc7b16de12d547d372a
Wojciech Przybyła <contact@wpww.pl>
2023-07-25 19:16:53 +0200

    colplot - low value labels more visible

--- packages/renderer/src/components/ColPlot.vue
+++ packages/renderer/src/components/ColPlot.vue
@@ -15,15 +15,19 @@ const props = withDefaults(defineProps<{
 
 let labels = [] as string[];
 let values = [] as number[];
+let heights = [] as number[];
 for(let el of props.data){
   labels.push(el[props.axes[0]]);
   values.push(el[props.axes[1]]);
 }
+const highest_col = Math.max(...values);
 if(props.flip){
   labels.reverse();
   values.reverse();
 }
-const highest_col = Math.max(...values);
+for(let i in props.data){
+  heights.push(values[i]/highest_col * props.barHeightPx)
+}
 </script>
 
 <template>
@@ -31,8 +35,8 @@ const highest_col = Math.max(...values);
     <h2 v-if="title">{{ title }}</h2>
 
     <div class="columns flex-right">
-      <div class="rounded flex-right h-center" v-for="(n, i) in values.length" :style="{ height: values[i]/highest_col * barHeightPx + 'px' }">
-        {{ asPln ? $toPln(values[i]) : values[i] }}
+      <div class="rounded" v-for="(n, i) in values.length" :style="{ height: heights[i] + 'px' }">
+        <span :class="{lowVal: heights[i] < 18}">{{ asPln ? $toPln(values[i]) : values[i] }}</span>
       </div>
     </div>
 
@@ -59,13 +63,19 @@ const highest_col = Math.max(...values);
 }
 .columns > div, .labels > span{
   width: 100%;
-  overflow: hidden;
   font-size: 0.75em;
 }
 .columns > div{
   background-color: hsla(var(--acc), 0.5);
+  position: relative;
 }
-.columns > div:hover{
-  background-color: hsla(var(--acc), 1);
+.columns span{
+  position: absolute;
+  top: 0;
+  width: 100%;
+  text-align: center;
+}
+.columns span.lowVal{
+  top: -18px;
 }
 </style>
