cab7d601fc101029bd98757c08deb95371a1f301
Wojciech Przybyła <contact@wpww.pl>
2023-08-07 22:39:54 +0200

    better looking loader

--- packages/renderer/src/components/Loader.vue
+++ packages/renderer/src/components/Loader.vue
@@ -1,6 +1,26 @@
+<script setup lang="ts">
+</script>
+
 <template>
-  <h2>
-    <fai :icon="['fas', 'cog']" spin />
-    Wczytuję...
-  </h2>
+  <div class="flex-down h-center v-center">
+    <span class="icon ghost">
+      <fai :icon="['fas', 'cog']" spin></fai>
+    </span>
+    <h2>Wczytuję...</h2>
+  </div>
 </template>
+
+<style scoped>
+.icon{
+  font-size: 4em;
+  padding: 0 0.5em;
+}
+span, h1, h2{
+  margin: 0;
+}
+h1::before, h1::after{
+  content: "===";
+  color: hsl(var(--acc));
+  padding: 0.5em;
+}
+</style>
