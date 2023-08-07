81a154e29809759f5d51ca2c1d4923d9f12b8a89
Wojciech Przybyła <contact@wpww.pl>
2023-08-07 19:19:46 +0200

    compact footer, worthy of a desktop app

--- packages/renderer/src/components/Footer.vue
+++ packages/renderer/src/components/Footer.vue
@@ -5,35 +5,40 @@ const APP_NAME = import.meta.env.VITE_APP_NAME;
 
 <template>
   <footer class="flex-right v-center">
-    <router-link to="/">
-      <img :src="logo" :alt="APP_NAME" />
-    </router-link>
-    <div id="app-info">
+    <div id="app-info" class="flex-right">
       <router-link to="/about">
         <h2>{{ APP_NAME }}</h2>
       </router-link>
       <p>Made by <a href="http://wpww.pl/" target="_blank">Wojciech Przybyła</a></p>
     </div>
+    <!-- <router-link to="/"> -->
+      <img :src="logo" :alt="APP_NAME" />
+    <!-- </router-link> -->
   </footer>
 </template>
 
 <style scoped>
 footer{
-  flex-direction: row-reverse;
+  flex-direction: row;
+  justify-content: space-between;
   background-color: hsl(0, 0%, 0%);
   color: hsl(0, 0%, 100%);
   flex-shrink: 0;
-  padding: 0.5em 1em;
+  padding: 0.2em 1em;
 }
 h2, p{
   margin: 0;
 }
+h2, img{
+  --height: 1.1em;
+  font-size: var(--height);
+  height: var(--height);
+}
 img{
-  height: 3em;
   filter: brightness(2)
 }
 
 #app-info{
-  text-align: right;
+  align-items: baseline;
 }
 </style>
