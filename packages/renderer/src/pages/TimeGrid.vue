a6dea604d5dd3a91687f672326fa906f0a477c4c
Wojciech Przybyła <contact@wpww.pl>
2023-09-14 23:14:03 +0200

    timegrid - optimized

--- packages/renderer/src/pages/TimeGrid.vue
+++ packages/renderer/src/pages/TimeGrid.vue
@@ -5,10 +5,36 @@ import Loader from '../components/Loader.vue';
 import { ref, onMounted } from 'vue';
 import { Session, Student } from 'types';
 import moment, { Moment } from 'moment';
+import SubPanel from '../components/SubPanel.vue';
+import Input from '../components/Input.vue';
 
 const students = ref([] as Student[]);
 const sessions = ref([] as Session[]);
+let sessions_f = ref([] as Session[]);
 const daterange = ref([] as Moment[]);
+const [filter_from, filter_to] = [
+  ref(moment().subtract(1, "month").format("YYYY-MM-DD")),
+  ref(moment().format("YYYY-MM-DD")),
+];
+const updateFilters = (name: string, val: string) => {
+  switch(name){
+    case "from": filter_from.value = val; break;
+    case "to": filter_to.value = val; break;
+  }
+  updateDataSet();
+}
+const updateDataSet = () => {
+  sessions_f.value = sessions.value.filter(ses =>
+    moment(ses.session_date).isBetween(filter_from.value, filter_to.value, undefined, "[]")
+  );
+
+  daterange.value = [];
+  let currentDate = moment(filter_to.value);
+  while(currentDate.isSameOrAfter(filter_from.value)){
+    daterange.value.push(currentDate.clone());
+    currentDate.subtract(1, "day");
+  }
+}
 
 onMounted(async () => {
   //students
@@ -20,6 +46,7 @@ onMounted(async () => {
       ORDER BY first_name, last_name`
     );
     students.value = data;
+    updateDataSet();
   }catch(err){
     console.error(err);
   }
@@ -32,23 +59,24 @@ onMounted(async () => {
       ORDER BY session_date DESC`
     );
     sessions.value = data;
-    const oldest = moment(sessions.value.slice(-1)[0].session_date);
-
-    let currentDate = moment();
-    while(currentDate.isSameOrAfter(oldest)){
-      daterange.value.push(currentDate.clone());
-      currentDate.subtract(1, "day");
-    }
-
   }catch(err){
     console.error(err);
   }
+
+  updateDataSet();
 });
 </script>
 
 <template>
   <PageHeader title="Obecność">
   </PageHeader>
+
+  <SubPanel title="Filtry">
+    <div class="flex-right h-center inputs-in-line">
+      <Input type="date" name="date_from" :value="filter_from" label="Od" @change="updateFilters('from', $event.target.value)"/>
+      <Input type="date" name="date_to" :value="filter_to" label="Do" @change="updateFilters('to', $event.target.value)" />
+    </div>
+  </SubPanel>
 
   <div v-if="students.length">
     <div id="timegrid" :style="{gridTemplateColumns: `auto repeat(${students.length}, 1fr)`}">
@@ -70,13 +98,13 @@ onMounted(async () => {
         </span>
         <span v-for="student in students"
           :class="{
-            marked: sessions.find((el: Session) => el.student_id == student.id && el.session_date == day.format('YYYY-MM-DD')),
+            marked: sessions_f.find((el: Session) => el.student_id == student.id && el.session_date == day.format('YYYY-MM-DD')),
             odd: day.month() % 2
           }"
           class="cell"
-          :title="sessions.find((el: Session) => el.student_id == student.id && el.session_date == day.format('YYYY-MM-DD')) && `${student.first_name} ${student.last_name}: ${sessions.find((el: Session) => el.student_id == student.id && el.session_date == day.format('YYYY-MM-DD'))!.duration} h`"
+          :title="sessions_f.find((el: Session) => el.student_id == student.id && el.session_date == day.format('YYYY-MM-DD')) && `${student.first_name} ${student.last_name}: ${sessions_f.find((el: Session) => el.student_id == student.id && el.session_date == day.format('YYYY-MM-DD'))!.duration} h`"
           >
-          {{ sessions.find((el: Session) => el.student_id == student.id && el.session_date == day.format('YYYY-MM-DD'))?.duration }}
+          {{ sessions_f.find((el: Session) => el.student_id == student.id && el.session_date == day.format('YYYY-MM-DD'))?.duration }}
         </span>
       </template>
     </div>
